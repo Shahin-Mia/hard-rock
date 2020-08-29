b1e06c0d3bbdf7dad38e5c1ca9f2c2b7079d33a6
Shahin-Mia <shahin.cmt2@gmail.com>
2020-08-29 16:12:14 +0600

    issues are fixed

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // search button function
-const searchResults = [];
 
 function getResult() {
     const allResults = document.getElementById("results-div");
@@ -7,6 +6,9 @@ function getResult() {
     allResults.style.display = "block";
     document.getElementById("lyrics-div").innerHTML = "";
     const searchInput = document.getElementById("search-input");
+    if (searchInput.value === "") {
+        allResults.innerHTML = `<h3>You didn't insert any Song Name or Artist name in Search box</h3>`;
+    }
     fetch(`https://api.lyrics.ovh/suggest/${searchInput.value}`)
         .then(res => res.json())
         .then(data => {
@@ -15,46 +17,39 @@ function getResult() {
                 const artistName = data.data[i].artist.name;
                 const id = data.data[i].id;
                 allResults.innerHTML += `<div class="single-result row align-items-center my-3 p-3">
-                                                                        <div class="col-md-9">
-                                                                        <h3 class="lyrics-name">${title}</h3>
-                                                                        <p class="author lead">Album by <span>${artistName}</span></p>
-                                                                        </div>
-                                                                        <div class="col-md-3 text-md-right text-center">
-                                                                        <button onClick="getLyrics(${id})" class="btn btn-success">Get Lyrics</button>
-                                                                        </div>
-                                                                    </div>`
-                searchResults.push({
-                    "id": data.data[i].id,
-                    "title": data.data[i].title,
-                    "artist": data.data[i].artist.name
-                })
+                                             <div class="col-md-9">
+                                                <h3 class="lyrics-name">${title}</h3>
+                                                <p class="author lead">Album by <span>${artistName}</span></p>
+                                             </div>
+                                            <div class="col-md-3 text-md-right text-center">
+                                                <button onClick="getLyrics('${title}', '${artistName}')" class="btn btn-success">Get Lyrics</button>
+                                            </div>
+                                        </div>`
             }
         })
 }
 
-function getLyrics(id) {
-    for (let i = 0; i < 10; i++) {
-        if (searchResults[i].id == id) {
-            const artistName = searchResults[i].artist;
-            const songTitle = searchResults[i].title;
-            fetch(`https://api.lyrics.ovh/v1/${artistName}/${songTitle}`)
-                .then(res => res.json())
-                .then(data => {
-                    if (data.lyrics == undefined) {
-                        data.lyrics = `Lyrics Not Found`;
-                    }
+function getLyrics(title, artist) {
+    fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
+        .then(res => res.json())
+        .then(data => {
+            if (data.lyrics == undefined) {
+                data.lyrics = `Lyrics Not Found`;
+            }
 
-                    document.getElementById('lyrics-div').innerHTML += ` <button onclick="goBack()" class="btn btn-outline-success go-back">&lsaquo;</button>
-                    <h2 class="text-success mb-4">${songTitle} - ${artistName}</h2>
+            document.getElementById('lyrics-div').innerHTML += ` <button onclick="goBack()" class="btn btn-outline-success go-back">&lsaquo;</button>
+                    <h2 class="text-success mb-4">${title} - ${artist}</h2>
                 <pre class="lyric text-white">${data.lyrics}</pre>`
-                })
-        }
-    }
+        })
+
     document.getElementById("results-div").style.display = "none";
+    document.getElementById("lyrics-div").style.display = "block";
 
 }
 
+
+//function for back button
 function goBack() {
-    document.getElementById("results-div").style.display = "block";
     document.getElementById("lyrics-div").style.display = "none";
+    getResult();
 }
